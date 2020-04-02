2b8a27873378746d7420f5dd7781cb6b51c78532
Fran Zekan <zekan.fran369@gmail.com>
2020-04-03 00:41:02 +0200

    Reorganise imports

--- client/src/components/EntriesTable.tsx
+++ client/src/components/EntriesTable.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { IEntry } from "../@types";
 import styled from "styled-components";
+
+import { IEntry } from "../@types";
 
 interface IEntriesTableProps {
   entries: IEntry[];
