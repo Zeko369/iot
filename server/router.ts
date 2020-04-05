880c1207cc15eb66b2072c167937af8e78fc5c6a
Fran Zekan <zekan.fran369@gmail.com>
2020-04-05 17:49:34 +0200

    Console.log device id for testing

--- server/router.ts
+++ server/router.ts
@@ -14,7 +14,15 @@ router.get("/", async (_req, res) => {
 
 router.post("/save", async (req, res) => {
   try {
-    const entry = await Entry.create(req.body);
+    const entity = {
+      temp: req.body.temp,
+      lux: req.body.lux,
+      hum: req.body.hum,
+    };
+
+    console.log(req.body.device_id);
+
+    const entry = await Entry.create(entity);
     res.status(201).send(entry.id);
   } catch (err) {
     res.status(500);
