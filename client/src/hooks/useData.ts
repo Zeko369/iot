c0dcf419cff0cdb002e331c4894c9dc2be885e32
Fran Zekan <zekan.fran369@gmail.com>
2020-04-05 23:02:01 +0200

    Fix ip for mobile

--- client/src/hooks/useData.ts
+++ client/src/hooks/useData.ts
@@ -1,6 +1,6 @@
 import { IState, Action } from "../@types";
 
-const URL = "http://localhost:5000/";
+const URL = "http://192.168.1.6:5000/";
 
 export const initState: IState = {
   loading: false,
