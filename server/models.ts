3ada38ec1c41663a79dc9c86fe0ef29b6e62e7a0
Fran Zekan <zekan.fran369@gmail.com>
2020-09-19 13:14:16 +0200

    TS stuff

--- server/models.ts
+++ server/models.ts
@@ -11,9 +11,21 @@ const entrySchema = new Schema({
   date: { type: Date, default: Date.now },
   temp: Number,
   lux: Number,
-  hum: Number
+  hum: Number,
+  device: { type: Schema.Types.ObjectId, ref: "Device" },
 });
 
 const Entry = model<IEntry>("Entry", entrySchema);
 
-export { entrySchema, Entry };
+export interface IDevice extends Document {
+  name: string;
+}
+
+const deviceSchema = new Schema({
+  name: String,
+  entries: [{ type: Schema.Types.ObjectId, ref: "Entry" }],
+});
+
+const Device = model<IDevice>("Device", deviceSchema);
+
+export { entrySchema, Entry, deviceSchema, Device };
